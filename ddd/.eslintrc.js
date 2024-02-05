65250196936f45fb42871c45afb954981bf22f7e
Gilvam Mourao <gilvamm@gmail.com>
2024-02-05 11:43:18 -0300

    feat(customer): entity com regras de negócio

---
+++ ddd/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'no-underscore-dangle': 'off',
+    'lines-between-class-members': 'off',
+  },
+};
---
+++ ddd/src/entity/customer.ts
@@ -0,0 +1,24 @@
+class Customer {
+  private _id: string;
+  private _name: string;
+  private _address: string;
+  private _active = true;
+
+  constructor(id: string, name: string, address: string) {
+    this._id = id;
+    this._name = name;
+    this._address = address;
+  }
+
+  changeName(name: string) {
+    this._name = name;
+  }
+
+  activate() {
+    this._active = true;
+  }
+
+  deactivate() {
+    this._active = false;
+  }
+}
