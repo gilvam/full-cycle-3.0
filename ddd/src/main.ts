46a579a271484aed01730c26f00c40f6185ee37a
Gilvam Mourao <gilvamm@gmail.com>
2024-02-09 10:29:23 -0300

    fix: jest with @swc

--- ddd/src/main.ts
+++ ddd/src/main.ts
@@ -8,11 +8,10 @@ const address = new Address('Rua dois', 2, '12345-678', 'São Paulo');
 customer.addAddress(address);
 customer.activate();
 
-const item1 = new OrderItem('1', 'Item 1', 10);
-const item2 = new OrderItem('2', 'Item 2', 15);
+const item1 = new OrderItem('1', 'Item 1', 10, 2, 'p1');
+const item2 = new OrderItem('2', 'Item 2', 15, 1, 'p2');
 
 const order = new Order('1', '123', [item1, item2]);
 
 console.log('customer: ', customer);
-
 console.log('order: ', order);
